bed3b6fd322148be6a119943f74f8a8c5be5e8a0
aharburg <connect@aaronharburg.com>
2017-10-28 17:41:23 +0200

    Codecademy generated from the project submission page.

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TrackList from '../TrackList/TrackList';
+import './SearchResults.css';
+
+class SearchResults extends Component {
+  render(){
+    return (
+    <div className="SearchResults">
+  <h2>Results</h2>
+  <TrackList onAdd={this.props.onAdd} tracks={this.props.searchResults}/>
+</div>
+  );
+  }
+}
+
+export default SearchResults;
